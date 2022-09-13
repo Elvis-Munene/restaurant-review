e6b1887e59184c490fb4be55f81d64c7ec764ae5
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-09-13 23:57:56 +0300

    modifies the ReviewForm component

---
+++ src/components/Restaurant/ReviewForm.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+
+function ReviewForm(){
+    return (
+    
+<div className="wrapper">
+    <form>
+        <div>Have an experience with [Restaurant Name]? Share your review</div>
+        <div className="field">
+            <input type="text" name="title"placeholder='Review Title'/>
+            </div>
+            <div className="field">
+                <input type="text" name="description" placeholder='Review Description'/>
+                </div>
+                <div className="field">
+                    <div className='rating-container'>
+                        <div className="rating-title-text">Rate This Airticle</div>[Star Rating Goes here]
+                    </div> 
+            </div>
+            <button type="submit">Submit Your Review</button>
+    </form>
+    </div>
+)
+    
+}
+
+
+export default ReviewForm;
+
+
